d5728b88052906c7c9bf3fb5452005c651d731f0
Pooja1663 <150426668+Pooja1663@users.noreply.github.com>
2025-01-30 12:09:41 +0530

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,15 @@
+// let div=document.createElement("div")
+// console.log(div);
+
+// document.body.appendChild(div)
+// div.innerHTML="<h1>Bangloreee</h1>"
+
+// //css
+// div.style.backgroundColor="blue"
+
+
+// let div=document.createElement("div")
+// console.log(div);
+
+// document.body.appendChild(div)
+//  div.innerHTML=
